b19478e004d6a4f195214c9ed57ce3072b3aa584
Sidharth <sidharthkumar2k19@gmail.com>
2024-02-15 23:48:13 +0530

    added Contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import styles from './contact.module.css'
+import Image from 'next/image'
 
 const ContactPage = () => {
   return (
-    <div>
-      ContactPage
-    </div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src='/contact.png' alt='' fill />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type='text' placeholder='Name and Surname' />
+          <input type='text' placeholder='Email Address' />
+          <input type='text' placeholder='Phone Number (Optional)' />
+          <textarea
+            name=''
+            id=''
+            cols={30}
+            rows={10}
+            placeholder='Message'
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div >
   )
 }
 
